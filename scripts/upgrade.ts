2de633f8f84bb852c06d9fc2018cc5705fbb334e
Antoni Kiszka <antonikiszka@icloud.com>
2021-11-29 20:33:43 +0100

    Upgrade on all chains.

--- scripts/upgrade.ts
+++ scripts/upgrade.ts
@@ -3,8 +3,10 @@ import hre, { ethers, upgrades } from "hardhat";
 async function main() {
   await hre.run('compile');
 
-  const TCPDataProxyAddress = "0xa398De2fEF0b37cf50c2F9D88b8953b94b49c78C";//"0xa398De2fEF0b37cf50c2F9D88b8953b94b49c78C";
-
+  // ropsten:    0x0D3E48e537F69d4BDbdc84a1A5BbD70Ad1fD0756
+  // bsctestnet: 0xa398De2fEF0b37cf50c2F9D88b8953b94b49c78C
+  const TCPDataProxyAddress = "0xa398De2fEF0b37cf50c2F9D88b8953b94b49c78C";
+  
   const TCPData = await hre.ethers.getContractFactory("TCPData");
   const tcpdata = await upgrades.upgradeProxy(TCPDataProxyAddress, TCPData);
   await tcpdata.deployed();
